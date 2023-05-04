11d3e1e589b5090537896dbfeb421882fe29b806
NatalyMolano <104031761+NatalyMolano@users.noreply.github.com>
2023-05-04 08:56:46 -0500

    Update triangulo.java

--- figurasg/src/main/java/figurasGeometricas/triangulo.java
+++ figurasg/src/main/java/figurasGeometricas/triangulo.java
@@ -1,33 +1,15 @@
 package figurasGeometricas;
 public class triangulo extends figura {
-    double base,altura,longitud,area,perimetro;
+    double base,altura;
     public triangulo()
    {
     
    }
 
-   public triangulo(double base, double altura, double longitud, double area, double perimetro) {
+   public triangulo(double base, double altura) {
         this.base = base;
         this.altura = altura;
-        this.longitud = longitud;
-        this.area = area;
-        this.perimetro = perimetro;
-    }
-
-    public double getArea() {
-        return area;
-    }
-
-    public void setArea(double area) {
-        this.area = area;
-    }
-
-    public double getPerimetro() {
-        return perimetro;
-    }
-
-    public void setPerimetro(double perimetro) {
-        this.perimetro = perimetro;
+        
     }
 
     public double getBase() {
@@ -42,22 +24,6 @@ public class triangulo extends figura {
     public void setAltura(double altura) {
         this.altura = altura;
     }
-    public double getLongitud() {
-        return longitud;
-    }
-    public void setLongitud(double longitud) {
-        this.longitud = longitud;
-    }
-    
-    public void calcularArea(double b, double a)
-    {
-        area = (a*b)/2;
-        System.out.println("El area del triangulo equilatero es: " + area);
-    }
-    public void calcularPerimetro(double l)
-    {
-        perimetro = l*3;
-        System.out.println("El perimetro del triangulo equilatero es: "+perimetro);
-    }
+   
     
 }
